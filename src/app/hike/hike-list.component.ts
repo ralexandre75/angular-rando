0aa4b5dbd548eb5538fadc88fabeec4986bcb5b3
rene.alexandre <rene.alexandre@grimoire-design.fr>
2018-09-06 15:02:15 +0200

    creer un component

---
+++ src/app/hike/hike-list.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-hike-list',
+    templateUrl: 'hike-list.component.html'
+})
+export class HikeListComponent {
+}
--- src/app/hike/hike.module.ts
+++ src/app/hike/hike.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HikeListComponent } from './hike-list.component';
 
 @NgModule({
     imports: [CommonModule],
-    declarations: [],
+    declarations: [
+        HikeListComponent
+    ],
     exports: [],
     providers: []
 })
