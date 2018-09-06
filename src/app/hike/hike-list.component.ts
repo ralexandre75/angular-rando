e486aad437c9cf703c38d52500cbf86d1015638c
rene.alexandre <rene.alexandre@grimoire-design.fr>
2018-09-06 16:23:31 +0200

    creer un service et l'injecter

--- src/app/hike/hike-list.component.ts
+++ src/app/hike/hike-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Hike } from './hike';
 
 @Component({
     selector: 'app-hike-list',
--- src/app/hike/hike.module.ts
+++ src/app/hike/hike.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HikeListComponent } from './hike-list.component';
+import { HikeService } from './hike.service';
 
 @NgModule({
     imports: [CommonModule],
-    declarations: [
-        HikeListComponent
-    ],
-    exports: [],
-    providers: []
+    declarations: [HikeListComponent],
+    exports: [HikeListComponent],
+    providers: [HikeService]
 })
 
 export class HikeModule {
