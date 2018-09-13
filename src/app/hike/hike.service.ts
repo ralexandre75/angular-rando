b254ddb99067c2c8cf64beb9394647b84b9e2bd8
rene.alexandre <rene.alexandre@grimoire-design.fr>
2018-09-13 16:26:37 +0200

    reactive programming dans le contexte de requetes http + operateur

--- src/app/hike/hike.service.ts
+++ src/app/hike/hike.service.ts
@@ -1,37 +1,22 @@
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
 import { Hike } from './hike';
+import { map } from 'rxjs/operators';
+import { tap } from 'rxjs/operators';
+
 
 @Injectable()
 export class HikeService {
-    hikes: Hike[] = [
-        {
-          'name': 'Saint-Aubin-du-Cormier',
-          'region': 'Bretagne',
-          'area': 'Ille-et-Vilaine',
-          'startingPoint': 'Plan d\'eau',
-          'distance': 12.8,
-          'distanceUnit': 'km',
-          'duration': 240,
-          'heightDifference': 267,
-          'description': '(depuis HikeService) Randonnée sympa, à démarrer par le plan d\'eau pour finir sur les hauteurs de Saint Aubin. Rochers imposants et des arbres magnifiques sont effectivement au rendez-vous. Bonne balade',
-          'evaluation': [4, 5, 3]
-        },
-        {
-          'name': 'Vallée du Couesnon',
-          'region': 'Bretagne',
-          'area': 'Ille-et-Vilaine',
-          'startingPoint': 'Château de la ville olivier',
-          'distance': 15.6,
-          'distanceUnit': 'km',
-          'duration': 270,
-          'heightDifference': 200,
-          /* tslint:disable:max-line-length */
-          'description': 'Il faut être en forme pour cette rando car il y a beaucoup de dénivelés. Mais le jeu en vaut la chandelle car admirer les bords du Couesnon est un régal. Bonne randonnée ',
-          'evaluation': [5, 5]
-        }
-      ];
 
-      getHikes() {
-          return this.hikes;
+    constructor(private _http: Http) {
+
+    }
+
+      // getHikes() {
+      //     return this.hikes;
+      // }
+
+      getHikesFromAPI() {
+        return this._http.get('app/api/hikes.json').pipe(tap(x => console.log(x)), map(hikes => hikes.json()));
       }
 }
