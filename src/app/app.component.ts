2aa22e9c7aef40678508023ed9ada30393983521
rene.alexandre <rene.alexandre@grimoire-design.fr>
2018-09-21 11:55:56 +0200

    afficher nos randonnées dans hike-list-component.html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { HttpModule } from '@angular/http';
 
 import { Hike } from './hike/hike';
@@ -10,18 +10,4 @@ import { HikeService } from './hike/hike.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  hikes: Hike[];
-
-  constructor(private _hikeService: HikeService) {
-  }
-
-  ngOnInit() {
-     // this.hikes = this._hikeService.getHikes();
-    this._hikeService.getHikesFromAPI().subscribe(
-                                              res => this.hikes = res,
-                                              err => console.error(err.status)
-    );
-    console.log(this.hikes);
-  }
-}
+export class AppComponent {}
--- src/app/hike/hike-list.component.ts
+++ src/app/hike/hike-list.component.ts
@@ -1,9 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 import { Hike } from './hike';
+import { HikeService } from './hike.service';
 
 @Component({
     selector: 'app-hike-list',
     templateUrl: 'hike-list.component.html'
 })
-export class HikeListComponent {
+export class HikeListComponent implements OnInit {hikes: Hike[];
+
+    constructor(private _hikeService: HikeService) {
+    }
+    ngOnInit() {
+       // this.hikes = this._hikeService.getHikes();
+      this._hikeService.getHikesFromAPI().subscribe(
+                                                res => this.hikes = res,
+                                                err => console.error(err.status)
+      );
+      console.log(this.hikes);
+    }
 }
