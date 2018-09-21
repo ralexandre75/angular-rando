e9f749c83653eb5f8712fe21c8eff1f685b7eab6
rene.alexandre <rene.alexandre@grimoire-design.fr>
2018-09-21 11:32:36 +0200

    mettre en oeuvre le routage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
+import { Route, RouterModule } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
 import { HikeModule } from './hike/hike.module';
 import { HomeModule } from './home/home.module';
+import { HomeComponent } from './home/home.component';
+import { HikeListComponent } from './hike/hike-list.component';
+import { PageNotFoundComponent } from './page-not-found.component';
+
+
+const APP_ROUTE: Route[] = [
+  { path: 'home', component: HomeComponent },
+  { path: 'hikes', component: HikeListComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
     HikeModule,
     HttpModule,
-    HomeModule
+    HomeModule,
+    RouterModule.forRoot(APP_ROUTE)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+
+
+
+export class AppModule {
+
+}
---
+++ src/app/page-not-found.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-pagenotfound',
+    template: `page non trouvé error 404`
+})
+
+export class PageNotFoundComponent {}
