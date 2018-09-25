d6458a54a4006d25fd5c6f7d5ea95e1ee1744574
rene.alexandre <rene.alexandre@grimoire-design.fr>
2018-09-25 10:48:02 +0200

    créer un lien incluant les paramètres 2

--- src/app/hike/hike.module.ts
+++ src/app/hike/hike.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
 import { HikeListComponent } from './hike-list.component';
 import { HikeService } from './hike.service';
 import { HikeDetailsComponent } from './hike-details.component';
 
+
 @NgModule({
-    imports: [CommonModule],
+    imports: [
+        CommonModule,
+        RouterModule
+    ],
     declarations: [
         HikeListComponent,
         HikeDetailsComponent
