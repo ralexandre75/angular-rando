973e5c39d8198a89061b55441e3ee02bbf574c66
ralexandre75 <rene.alexandre@grimoire-design.fr>
2018-09-28 09:33:05 +0200

    le two way binding à l'aide de [(ngModel)]

--- src/app/hike/hike.module.ts
+++ src/app/hike/hike.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { HikeListComponent } from './hike-list.component';
 import { HikeService } from './hike.service';
@@ -10,7 +11,8 @@ import { HikeDetailsComponent } from './hike-details.component';
 @NgModule({
     imports: [
         CommonModule,
-        RouterModule
+        RouterModule,
+        FormsModule
     ],
     declarations: [
         HikeListComponent,
