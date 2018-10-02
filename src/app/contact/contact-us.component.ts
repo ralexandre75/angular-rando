ba57f4dae57d375be419da83a933d79085ac8021
ralexandre75 <rene.alexandre@grimoire-design.fr>
2018-10-02 11:49:33 +0200

    Gérer l'événement de soumission du formulaire

--- src/app/contact/contact-us.component.ts
+++ src/app/contact/contact-us.component.ts
@@ -1,8 +1,13 @@
 import { Component } from "@angular/core";
+import { NgForm } from "@angular/forms";
 
 @Component({
     templateUrl: 'contact-us.component.html'
 })
 export class ContactUsComponent {
     isVisible: boolean = false;
+
+    sendMessage(form: NgForm) {
+        console.log(form.value);
+    }
 }
