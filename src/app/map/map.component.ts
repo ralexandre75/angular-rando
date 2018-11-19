66224f0640af0ed7b18447eeef076f9c9493b462
rcvfzuc@2101XY <ralexandre75>
2018-11-19 11:04:35 +0100

    gérer les événement d'une google map - récupérer dynamiquement des coordonnées GPS

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -11,4 +11,19 @@ import { Component } from '@angular/core';
 export class MapComponent {
     lat: number = 48.30740;
     lng: number = -1.43276;
+
+    droppedLat: number;
+    droppedLng: number;
+
+    markerWasDropped : boolean;
+
+    onCoordMarkerDropped(event: any) {
+        console.log(event);
+
+        this.markerWasDropped = true;
+        this.droppedLat = event.coords.lat.toFixed(5);
+        this.droppedLng = event.coords.lng.toFixed(5);
+    }
+
+    
 }
