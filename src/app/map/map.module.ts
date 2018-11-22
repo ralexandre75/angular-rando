b8e5330e45417edc02e3819905921103d1a7f9ec
rcvfzuc@2101XY <ralexandre75>
2018-11-22 10:29:05 +0100

    Créer un pseudo formulaire à partir de sa longitude et de sa latitude

--- src/app/map/map.module.ts
+++ src/app/map/map.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { AgmCoreModule } from '@agm/core';
 import { MapComponent } from './map.component';
 import { MapService } from './map.service';
+import { FormsModule } from '@angular/forms';
 
 
 
 @NgModule({
     imports: [
         CommonModule,
+        FormsModule,
         AgmCoreModule.forRoot({
             apiKey: 'AIzaSyAew4Z261E-iPAf9iVVDcBXOhBIiC7Py5Q'
           })
